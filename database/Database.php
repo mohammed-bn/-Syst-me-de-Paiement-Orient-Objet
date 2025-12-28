7c12cb7418bc7183cf1f7a50a99e37e0abaaa8fb
mohammed-bn <mbentiss11@gmail.com>
2025-12-29 00:48:51 +0100

    la partir de conexion avec data base

--- database/Database.php
+++ database/Database.php
@@ -1,30 +1,32 @@
 <?php
-    class Database{
+class Database{
 
-        private $host = "localhost";
-        private $username = "root";
-        private $password = "";
-        private $dbname =  "systeme_paiement";
-        private $conn;
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $dbname =  "systeme__paiement";
+    private $conn;
 
-        private function conn(){
-            try {
-                $this->conn = new PDO(
-                    "mysql:host=" . $this->host . ";dbname=" . $this->dbname,
-                    $this->username,
-                    $this->password,
-                );
-                
-                echo "conexion a ete avec un succée";
-        }catch(PDOException $e){
+    private function conn(){
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->dbname,
+                $this->username,
+                $this->password
+            );
+            // echo "conexion a ete avec un succée\n";
+        } catch(PDOException $e){
             die("Erreur du serveur!") ;
         }
-        }
-
-        public function __construct(){
-            $this->conn();
-        }
     }
-    $conn = new Database();
-    var_dump($conn);
-?>
+    public function get_connection() {
+        return $this->conn;
+    }
+
+    public function __construct(){
+        $this->conn();
+    }
+
+}
+    // var_dump("nadi canadi",$conn);
+
